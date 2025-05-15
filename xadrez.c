061d9b0dc6a381aeb9f17aa22a19d4bd07e44b3b
Eduardo Gomes de Castro Terra <eduardoterragomes@gmail.com>
2025-05-15 00:21:09 +0000

    adicionado as variáveis constantes

--- xadrez.c
+++ xadrez.c
@@ -7,15 +7,19 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    const int maxCasasBispo = 5, maxCasasTorre = 5, maxCasasRainha = 8;
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
